8dcf2d15fc8fabff0f3658652affb1ba16201927
conradof <cvera@fractalcs.com>
2017-10-03 10:54:34 -0500

    Activador jax rs

---
+++ src/main/java/com/github/conradovera/activador/ActivadorJaxRs.java
@@ -0,0 +1,9 @@
+package com.github.conradovera.activador;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/servicios")
+public class ActivadorJaxRs extends Application{
+
+}
